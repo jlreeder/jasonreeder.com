bd7ff4e67a3c589058d1eb54bbe337a88aeae018
Jason Reeder <info@jasonreeder.com>
2016-08-22 08:55:23 -0700

    Delete unused code

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,13 +19,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (("Pelican", "http://getpelican.com/"),
-#          ("Python.org", "http://python.org/"),
-#          ("Jinja2", "http://jinja.pocoo.org/"),
-#          ("You can modify those links in your config file", "#"),
-#         )
-
 # Social widget
 SOCIAL = (("linkedin", "https://www.linkedin.com/in/jason-reeder-07481b52"),
           ("github", "https://github.com/jlreeder"),
